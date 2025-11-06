7d76077389550b4debce85d9d1965d3618abb000
Aryan Gupta <ag980455@gmail.com>
2025-11-07 02:03:10 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import "react-native-gesture-handler";
+import "react-native-reanimated";
+
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import GameScreen from "./src/screens/GameScreen";
+
+export default function App() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <StatusBar style="light" />
+      <GameScreen />
+    </GestureHandlerRootView>
+  );
+}
